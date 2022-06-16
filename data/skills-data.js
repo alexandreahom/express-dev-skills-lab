17bd95e3668f8c266537a07e4c4ec6537d724f42
Alexandrea <lexhom24@gmail.com>
2022-06-15 20:38:24 -0400

    add more skills

--- data/skills-data.js
+++ data/skills-data.js
@@ -1,7 +1,9 @@
 const skills = [
   {text: 'Swimming', done: true },
+  {text: 'Snowboarding', done: false},
   {text: 'Gaming', done: true },
   {text: 'Streaming', done: true},
+  {text: 'Typing', done: false},
 ]
 
 export { 
